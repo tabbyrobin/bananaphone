e9d7e0a4a3e7ab9af90da528fcdb452e506e6834
Anon <anon@example.com>
2020-11-05 22:34:44 -0800

    Make installable with pip2

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bananaphone',
+      version='0.0.1',
+      description='',
+      url='',
+      author='',
+      author_email='',
+      license='',
+      packages=['bananaphone'],
+      zip_safe=False)
